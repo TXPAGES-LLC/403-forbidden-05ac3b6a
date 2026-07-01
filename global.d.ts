0fd52f2bf1ad84dbc39fe7de0c38a23caa7603d2
TXPAGES-LLC <mark@txpages.com>
2026-07-01 15:04:44 -0500

    Auto-fix attempt 2

--- global.d.ts
+++ global.d.ts
@@ -1,6 +1,8 @@
+// Allow importing plain CSS files (side-effect imports like import './globals.css')
+// Also include common module patterns so TS doesn't error during next build.
 declare module '*.css';
 declare module '*.scss';
 declare module '*.sass';
 declare module '*.module.css';
 declare module '*.module.scss';
-export {};
+declare module '*.module.sass';
